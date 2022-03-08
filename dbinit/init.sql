8cc560f9d21f627d2f74e418fec77dd7ed814d90
abassGarane <thelibertinewreck@gmail.com>
2022-03-08 16:10:48 +0300

    produre for creating users

--- dbinit/init.sql
+++ dbinit/init.sql
@@ -8,9 +8,26 @@ CREATE TABLE patients(
     email VARCHAR(255) DEFAULT NULL,
     phone VARCHAR(255) DEFAULT NULL,
     address VARCHAR(255) DEFAULT NULL,
-    dignosis VARCHAR(255) DEFAULT NULL,
+    diagnosis VARCHAR(255) DEFAULT NULL,
     image_url VARCHAR(255) DEFAULT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(id),
     CONSTRAINT UQ_Patients_Email UNIQUE(email)
 );
+
+
+DELIMITER // 
+    CREATE PROCEDURE create_and_return(
+    IN first_name VARCHAR(255),
+    IN last_name VARCHAR(255),
+    IN email VARCHAR(255), 
+    IN phone VARCHAR(255), 
+    IN address VARCHAR(255),
+    IN diagnosis VARCHAR(255),
+    IN email_url VARCHAR(255))
+    BEGIN
+    INSERT INTO patients(first_name,last_name,email,phone,address,diagnosis,image_url) VALUES(first_name,last_name,email,phone,address,diagnosis,image_url);
+    SET @PATIENT_ID=LAST_INSERT_ID();
+    SELECT * FROM patients where id=@PATIENT_ID;
+    END //
+DELIMITER;
